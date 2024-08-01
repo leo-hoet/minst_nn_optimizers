c4dd56c15200080f0c35080488e00b91a3892cca
Leo Hoet <leohoet98@gmail.com>
2024-08-01 16:08:06 -0300

    Inital commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+
+import tensorflow as tf
+
+from model import NNModel
+
+# Load and preprocess the MNIST dataset
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+# Normalize pixels between 0 and 1
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Flatten the images
+x_train = x_train.reshape(x_train.shape[0], 784)
+x_test = x_test.reshape(x_test.shape[0], 784)
+
+model = NNModel()
+
+def run():
+    t0 = time.perf_counter()
+    model.randomize_weights()
+    metrics = model.metrics(x_test, y_test)
+    t1 = time.perf_counter()
+    print(f"ELAPSED TIME {t1-t0} ms {metrics=}")
+
+for i in range(10):
+    run()
+
+
+
+
+
---
+++ model.py
@@ -0,0 +1,72 @@
+import numpy as np
+from keras import Input, Model
+from keras.src.layers import Dense
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+
+
+def _random_weights():
+    return [
+        [],
+        [np.random.rand(784, 128), np.random.rand(128)],
+        [np.random.rand(128, 64), np.random.rand(64)],
+        [np.random.rand(64, 10), np.random.rand(10)]
+    ]
+
+
+class NNModel:
+    def __init__(self):
+        inputs = Input(shape=(784,))
+        x = Dense(128, activation='relu')(inputs)
+        x = Dense(64, activation='relu')(x)
+        outputs = Dense(10, activation='softmax')(x)
+        model = Model(inputs=inputs, outputs=outputs, name="mnist_model")
+
+        # Compile the model (we won't use this for training, but it's required to use the model)
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        self.model = model
+        self.randomize_weights()
+
+    def randomize_weights(self):
+        self.set_custom_weights(_random_weights())
+
+    def set_custom_weights(self, weights):
+        for layer, w in zip(self.model.layers, weights):
+            layer.set_weights(w)
+
+    def predict_digit(self, image):
+        image = image.reshape(1, 784)
+        prediction = self.model.predict(image)
+        return np.argmax(prediction)
+    
+    def get_weights_as_numpy(self):
+        weights_list = []
+        for layer in self.model.layers:
+            layer_weights = layer.get_weights()
+            for w in layer_weights:
+                weights_list.append(w.flatten())
+        return np.concatenate(weights_list)
+
+    def metrics(self, X_test, y_true):
+        # Reshape X_test if necessary
+        if X_test.ndim == 3:
+            X_test = X_test.reshape(-1, 784)
+
+        # Get predictions
+        y_pred = self.model.predict(X_test)
+        y_pred_classes = np.argmax(y_pred, axis=1)
+
+        # Calculate metrics
+        accuracy = accuracy_score(y_true, y_pred_classes)
+        precision = precision_score(y_true, y_pred_classes, average='weighted')
+        recall = recall_score(y_true, y_pred_classes, average='weighted')
+        f1 = f1_score(y_true, y_pred_classes, average='weighted')
+
+        return {
+            'accuracy': accuracy,
+            'precision': precision,
+            'recall': recall,
+            'f1_score': f1
+        }
